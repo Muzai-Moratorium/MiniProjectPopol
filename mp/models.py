250bc2e3b4715f94c8a0a13501b10d1922c3ae15
User <wlgn115@icloud.com>
2025-12-12 16:57:14 +0900

    카카오맵 마커와 교통상태 추가

--- mp/models.py
+++ mp/models.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_security import UserMixin, RoleMixin
+from datetime import datetime
 import uuid
 
 db = SQLAlchemy()
@@ -26,3 +27,4 @@ class User(db.Model, UserMixin):
     active = db.Column(db.Boolean())
     fs_uniquifier = db.Column(db.String(255), unique=True, nullable=False, default=lambda: str(uuid.uuid4()))
     roles = db.relationship('Role', secondary=user_roles, backref='users')
+
